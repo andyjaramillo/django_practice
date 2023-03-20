e8b65ce0a7458e07ea61b8a46eab378583fed281
root <andy.jaramillo022@gmail.com>
2023-03-20 11:08:57 -0400

    added Notes table in database

---
+++ notes/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from . import models
+# Register your models here.
+#modifies the django admin interface
+class NotesAdmin(admin.ModelAdmin):
+    pass
+#adds the notes to the admin page
+admin.site.register(models.Notes, NotesAdmin) 
